4a08cd0dc6025fe7dacb6fb2ff72eb6e70ea5286
Aurimukstis1 <71183486+Aurimukstis1@users.noreply.github.com>
2025-05-22 14:33:28 +0300

    release patch 1.0.2

--- updater.py
+++ updater.py
@@ -2,14 +2,15 @@ import os
 import requests
 
 def download_latest_release():
-    github_url = "https://github.com/Aurimukstis1/nationwider-git/dist/main.exe"
+    github_url = "https://github.com/Aurimukstis1/nationwider-git/raw/refs/heads/main/dist/main.exe"
     try:
         response = requests.get(github_url)
         response.raise_for_status()
         with open("new_version.exe.tmp", "wb") as f:
             f.write(response.content)
-        print("Downloaded new version")
+        print("replacing ...")
         # replace old exe
+        os.remove("main.exe")
         os.rename("new_version.exe.tmp", "main.exe")
     except Exception as e:
         print(f"Error: {e}")
